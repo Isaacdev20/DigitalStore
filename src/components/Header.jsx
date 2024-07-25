f056fba4f569e31ef0524819b8bf4ad8ff80bb7d
Isaac <147888413+Isaacdev20@users.noreply.github.com>
2024-07-25 15:06:25 -0300

    colocando a imagem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+
 export default function Header({children, className}){
     return(
         <header className={className}>
-            {children}
+            <div  className=" flex flex-row">
+                <img src="/public/download.png" alt="fotoDaLogo" className="w-[253px] h-[44px]" />
+                {children}
+            </div>
         </header>
     )
 }
